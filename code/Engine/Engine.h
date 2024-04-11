23990efaeab7a2fd6bae80c60f65c9bdb4800625
zrebas <zuzanna.rebas@gmail.com>
2024-04-11 20:06:18 +0200

    refactor commit while it works

--- code/Engine/Engine.h
+++ code/Engine/Engine.h
@@ -6,7 +6,20 @@
 #include "imgui_impl/imgui_impl_opengl3.h"
 
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-
+//#define PROFILER
+#if defined(PROFILER) //overloading operators new and delete globally for profiling
+void* operator new(std::size_t count)
+{
+    auto ptr = malloc(count);
+    TracyAlloc(ptr, count);
+    return ptr;
+}
+void operator delete(void* ptr) noexcept
+{
+    TracyFree(ptr);
+    free(ptr);
+}
+#endif
 #include "GLFW/glfw3.h"
 #include "glad/glad.h"
 #include "stb_image.h"
@@ -33,18 +46,31 @@ struct Settings{
     int32_t GL_VERSION_MAJOR = 4;
     int32_t GL_VERSION_MINOR = 6;
 
-    //ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     std::string jsonSettingsFilePath = "res/content/maps/ExampleNotWorkingScene.json";
 
     float lastX = WINDOW_WIDTH / 2.0f;
     float lastY = WINDOW_HEIGHT / 2.0f;
     bool firstMouse = true;
-    //Camera camera;
 
     float deltaTime = 0.0f;	// time between current frame and last frame
     float lastFrame = 0.0f;
     Camera camera;
 } s;
+
+//float speed = 5;
+///!!!!!----------------------------->
+//USE THIS TO GET A TRANSFORM FROM A JSON
+JsonReader j(s.jsonSettingsFilePath);
+
+//Transform* CreateTransform(std::string pathToObjectInJson)
+//{
+//    return new Transform(new Model(j.ParseToString(pathToObjectInJson, "modelPath")),
+//                         j.ParseToVec3(pathToObjectInJson, "pos"),
+//                         j.ParseToVec3(pathToObjectInJson, "rot"),
+//                         j.ParseToVec3(pathToObjectInJson, "scale"));
+//}
+
+
 
 int GLFWInit()
 {
@@ -59,8 +85,6 @@ int GLFWInit()
     return 0;
 }
 
-
-
 void init_imgui()
 {
     // Setup Dear ImGui binding
@@ -73,61 +97,26 @@ void init_imgui()
     ImGui_ImplGlfw_InitForOpenGL(s.window, true);
     ImGui_ImplOpenGL3_Init(s.glsl_version);
 
-    // Setup style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
 }
-
-
-///!!!!!----------------------------->
-//USE THIS TO GET A TRANSFORM FROM A JSON
-JsonReader j(s.jsonSettingsFilePath);
-
-//Transform* CreateTransform(std::string pathToObjectInJson)
-//{
-//    return new Transform(new Model(j.ParseToString(pathToObjectInJson, "modelPath")),
-//                         j.ParseToVec3(pathToObjectInJson, "pos"),
-//                         j.ParseToVec3(pathToObjectInJson, "rot"),
-//                         j.ParseToVec3(pathToObjectInJson, "scale"));
-//}
 
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-float speed = 5;
-
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    //if(carMovement) return;
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
     {
-
         s.camera.ProcessKeyboard(FORWARD, s.deltaTime);
     }
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
     {
-
         s.camera.ProcessKeyboard(BACKWARD, s.deltaTime);
     }
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
@@ -136,7 +125,6 @@ void processInput(GLFWwindow *window)
     }
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
     {
-
         s.camera.ProcessKeyboard(RIGHT, s.deltaTime);
     }
     if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
@@ -157,7 +145,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
-
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
@@ -212,4 +199,56 @@ void imgui_begin()
     ImGui::NewFrame();
 }
 
+void init(){
+    //GLFW INITIALIZATION
+    if (GLFWInit())
+    {
+        LOG_ERROR("Failed to initialize GLFW");
+        exit(-1);
+    }
+    LOG_INFO("GLFW initialized");
+
+    //CREATING WINDOW
+    s.window = glfwCreateWindow(s.WINDOW_WIDTH, s.WINDOW_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (s.window == nullptr)
+    {
+        LOG_ERROR("Failed to Create GLFW window");
+        glfwTerminate();
+        exit(-1);
+    }
+    LOG_INFO("GLFW window created");
+    glfwMakeContextCurrent(s.window);
+    glfwSwapInterval(1); //how often buffers will swap
+
+    //GLAD INITIALIZING
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        LOG_ERROR("Failed to initialize GLAD");
+        glfwDestroyWindow(s.window);
+        glfwTerminate();
+        exit(-1);
+    }
+    LOG_INFO("GLAD initialized");
+
+    //SETTING OPENGL PARAMETERS
+    glEnable(GL_DEPTH_TEST); //enabling deoth tests
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); //determines how OpenGL renders the edges and interiors of polygons
+    SetCallbacks(s.window);//setting the callback functions for the window
+
+    //INITIALIZING IMGUI
+    init_imgui();
+
+    //INITIALIZING PROFILER
+    #if defined(PROFILER)
+        Profiler::get().init();
+    #endif
+}
+void end(){
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(s.window);
+    glfwTerminate();
+}
 #endif //ENGINE_ENGINE_H
--- code/main.cpp
+++ code/main.cpp
@@ -4,20 +4,7 @@
 #include "tracy/TracyOpenGL.hpp"
 #include "Text/Text.h"
 
-//#define PROFILER
-#if defined(PROFILER) //overloading operators new and delete globally for profiling
-void* operator new(std::size_t count)
-{
-    auto ptr = malloc(count);
-    TracyAlloc(ptr, count);
-    return ptr;
-}
-void operator delete(void* ptr) noexcept
-{
-    TracyFree(ptr);
-    free(ptr);
-}
-#endif
+
 
 #include "Engine/Engine.h"
 #include "Editor/Editor.h"
@@ -30,76 +17,24 @@ void operator delete(void* ptr) noexcept
 using namespace SceneManagement;
 
 int main() {
-    #if defined(PROFILER)
-        Profiler::get().init();
-    #endif
 
-	if (GLFWInit())
-	{
-		LOG_ERROR("Failed to initialize GLFW");
-		return -1;
-	}
-
-	LOG_INFO("GLFW initialized");
-
+    init();
 	//Radek note: don't mind me, just testing
 #pragma region TEST
 	EditorLayer::Editor editor;
     editor.init(&s.camera);
     CollisionManager cm;
-
 	AudioManager a;
 	a.init();
-
 	Sound* sound = a.loadSound("res/content/sounds/Ich will.mp3");
 	SceneManager sm;
-
 	sm.loadScene("res/content/maps/exampleScene.yaml");
 	//sound->play();
-
 	//! THEE WHO SHALL FIND THIS VOLUME VALUE,
 	//! BE AWARE OF CONSEQUENCES STANDING BEHIND ALTERING IT
 	sound->setVolume(2.f);
-
 	LOG_INFO("If u hear germans singing, that's a good sing.");
-
-
 #pragma endregion TEST
-
-	s.window = glfwCreateWindow(s.WINDOW_WIDTH, s.WINDOW_HEIGHT, "LearnOpenGL", NULL, NULL);
-
-	if (s.window == nullptr)
-	{
-		LOG_ERROR("Failed to Create GLFW window");
-		glfwTerminate();
-		return -1;
-	}
-
-	LOG_INFO("GLFW window created");
-	glfwMakeContextCurrent(s.window);
-	glfwSwapInterval(1);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		LOG_ERROR("Failed to initialize GLAD");
-
-		glfwDestroyWindow(s.window);
-		glfwTerminate();
-
-		return -1;
-	}
-
-	LOG_INFO("GLAD initialized");
-
-
-	//    stbi_set_flip_vertically_on_load(true);
-
-	glEnable(GL_DEPTH_TEST);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	SetCallbacks(s.window);
-
-	init_imgui();
 
     Text* arcadeRenderer = new Text("res/content/fonts/ARCADECLASSIC.TTF");
     arcadeRenderer->setParameters("dupa", 100, 100, 1.0f, glm::vec3(0.5, 0.8f, 0.2f), (float) s.WINDOW_WIDTH,
@@ -108,12 +43,9 @@ int main() {
 
 	Scene2 scene("scene");
 	Entity* entity = new Entity("nanosuit");
-	Entity* monke = new Entity("monke");
     Entity* player = new Entity("player");
-	// Entity* airplane = new Entity("airplane");
 
 	Model* model = new Model("res\\content\\models\\nanosuit\\nanosuit.obj");
-	Model* monkeModel = new Model("res\\content\\models\\plane.obj");
     Model* playerModel = new Model("res\\content\\models\\player.obj");
     Model* sphere = new Model("res/content/models/sphere/untitled.obj");
 
@@ -125,23 +57,17 @@ int main() {
 	// Model* airplaneModel = new Model("res\\content\\models\\aircraft\\airplane.obj");
 
 	entity->addComponent(model);
-	monke->addComponent(monkeModel);
     player ->addComponent(playerModel);
     //player->addComponent(playerCamera);
-	// airplane->addComponent(airplaneModel);
 	scene.addEntity(entity);
-	scene.addEntity(monke);
     scene.addEntity(player);
-	// scene.addEntity(airplane);
 
 	Shader shader("res/content/shaders/vertex.glsl", "res/content/shaders/fragment.glsl");
     Shader collisionTestShader("res/content/shaders/vertex.glsl", "res/content/shaders/collisionTest.frag");
-    Shader shaderText("res/content/shaders/vertexText.glsl", "res/content/shaders/fragmentText.glsl");
 	Renderer renderer;
     renderer.init();
 
     float yrotation = 0;
-	monke->getTransform()->setPosition(glm::vec3(5, 3, 1));
     player->getTransform()->setPosition(glm::vec3(-5, -2, 1));
 
     sm.getLoadedScenes()[0]->getSceneEntities()[0]->addComponent(cc1);
@@ -199,11 +125,11 @@ int main() {
 
         if(cc1->germansSing)
         {
-            sound->play();
+            //sound->play();
         }
         else
         {
-            sound->stop();
+            //sound->stop();
         }
 
 		Profiler::get().markFrame();
@@ -218,20 +144,8 @@ int main() {
 		glfwPollEvents();
 	}
 
-#if defined(PROFILER)
-    Profiler::get().end();
-#endif
-
     a.end();
     AssetManager::end();
-
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-
-	glfwDestroyWindow(s.window);
-	glfwTerminate();
-
-
+	end();
 	return 0;
 }
