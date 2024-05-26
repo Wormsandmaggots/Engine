e62e3d054fcbc6af9fd5bfc0199e5193ea0b4432
zrebas <zuzanna.rebas@gmail.com>
2024-05-26 17:38:43 +0200

    .

--- code/main.cpp
+++ code/main.cpp
@@ -2,12 +2,11 @@
 #include "Debug/Profiler.h"
 #include "tracy/TracyOpenGL.hpp"
 #include "Text/Text.h"
+
 #include "Engine/Engine.h"
 #include "Editor/Editor.h"
 #include "Core/AssetManager/AssetManager.h"
 #include "ThirdPersonCamera.h"
-#include "RigPrep.h"
-#include "Animation/InverseKinematics.h"
 #include "Physics/ColliderComponent.h"
 #include "Physics/CollisionManager.h"
 #include "Editor/Gizmos.h"
@@ -17,24 +16,21 @@
 #include "Renderer/MaterialAsset.h"
 #include "Renderer/FrameBuffer.h"
 #include "Renderer/SSAO.h"
-#include "Animation/Animation.h"
-#include "Animation/Animator.h"
-
 using namespace SceneManagement;
 
 int main() {
     init();
-	EditorLayer::Editor editor;
+    EditorLayer::Editor editor;
     editor.init(&s.camera);
     CollisionManager cm;
-	AudioManager a;
-	a.init();
-	Sound* sound = a.loadSound("res/content/sounds/Ich will.mp3");
-	SceneManager sm;
+    AudioManager a;
+    a.init();
+    Sound* sound = a.loadSound("res/content/sounds/Ich will.mp3");
+    SceneManager sm;
 
-	sm.loadScene("res/content/maps/test.yaml");
+    sm.loadScene("res/content/maps/test.yaml");
 
-	sound->setVolume(2.f);
+    sound->setVolume(2.f);
 
     //HID - test
     //TODO: Kuba: Czy to może tutaj zostać?
@@ -43,21 +39,20 @@ int main() {
     DebugInput debugInput;
     //HID - test
 
-	Shader shader("res/content/shaders/vertex.glsl", "res/content/shaders/fragment.glsl");
-	Shader collisionTestShader("res/content/shaders/vertex.glsl", "res/content/shaders/collisionTest.frag");
-	Shader shaderText("res/content/shaders/vertexText.glsl", "res/content/shaders/fragmentText.glsl");
+    Shader shader("res/content/shaders/vertex.glsl", "res/content/shaders/fragment.glsl");
+    Shader collisionTestShader("res/content/shaders/vertex.glsl", "res/content/shaders/collisionTest.frag");
+    Shader shaderText("res/content/shaders/vertexText.glsl", "res/content/shaders/fragmentText.glsl");
     Shader colorShader("res/content/shaders/color_v.glsl", "res/content/shaders/color_f.glsl");
     Shader shaderPbr("res/content/shaders/vertexPbr.glsl", "res/content/shaders/fragmentPbr.glsl");
     Shader shaderCel("res/content/shaders/vertex.glsl", "res/content/shaders/fragmentCel.glsl");
     Shader screenShader("res/content/shaders/framebuffer.vert", "res/content/shaders/framebuffer.frag");
     //TODO: Kuba: Czy to może tutaj zostać?
-    Shader shaderRig("res/content/shaders/vertexRig.glsl", "res/content/shaders/fragment.glsl");
 
     //HUD
     ProgressBar progressBar("res/content/shaders/vertex_2d.glsl", "res/content/shaders/progress_bar_fragment.glsl", "res/content/textures/bar.png", 100.0f);
     BackgroundImage backgroundImage("res/content/shaders/vertex_2d.glsl", "res/content/shaders/fragment_2d.glsl", "res/content/textures/nodes.png");
     Image image("res/content/shaders/vertex_2d.glsl", "res/content/shaders/fragment_2d.glsl", "res/content/textures/hud_back.png");
-    
+
     MaterialAsset material("res/content/materials/color.json");
 
     SSAO ssao;
@@ -66,27 +61,22 @@ int main() {
     Renderer renderer(&ssao.shaderGeometryPass);
     renderer.init();
 
-	renderer.addShader(&collisionTestShader);
+    renderer.addShader(&collisionTestShader);
     renderer.addShader(&colorShader);
     renderer.addShader(material.getShader());//TODO: Automatyczne dodawanie shadera do updatowania MVP
-    renderer.addShader(&shaderRig);
 
-    Model* player = new Model("res/content/models/Character_rigged/Character_base_rig.fbx", &shaderRig);
-    RigPrep* playerRig = new RigPrep(player);
-    InverseKinematics* playerIK = new InverseKinematics(playerRig);
+    Model* player = new Model("res/content/models/player/character_base.obj");
 
-    int offset = 0;
-	renderer.addShader(&shaderText);
+    renderer.addShader(&shaderText);
     renderer.addShader(&shaderPbr);
     renderer.addShader(&shaderCel);
     renderer.addShader(&ssao.shaderGeometryPass);
 
-    //Model* club = new Model("res/content/models/club2/club2.obj", &shaderPbr);
-	Model* sphere = new Model("res\\content\\models\\sphere\\untitled.obj", &collisionTestShader);
-    Model* player2 = new Model("res/content/models/npc1/pykpykKurwaMaDzialac.fbx", &shaderRig);
-    Animation* npcAnimation = new Animation("res/content/models/npc1/pykpykKurwaMaDzialac.fbx", player2);
-    RigPrep* npcRig = new RigPrep(player2);
-    Animator* animator = new Animator(npcAnimation);
+    Model* club = new Model("res/content/models/club2/club2.obj", &ssao.shaderGeometryPass);
+    Model* sphere = new Model("res\\content\\models\\sphere\\untitled.obj", &ssao.shaderGeometryPass);
+    //Model* player = new Model("res\\content\\models\\player\\character_base.obj", &shaderPbr);
+    Model* player2 = new Model("res/content/models/random.fbx", &ssao.shaderGeometryPass);
+
     Text* arcadeRenderer = new Text("res/content/fonts/ARCADECLASSIC.TTF");
     Text* counterRenderer = new Text("res/content/fonts/ARCADECLASSIC.TTF");
 
@@ -98,12 +88,11 @@ int main() {
 
     cc1->start();
     cc2->start();
-/*
+
     Entity* player1 = new Entity("player");
-	sm.getLoadedScenes()[0]->addEntity(player1);
+    sm.getLoadedScenes()[0]->addEntity(player1);
     player1->addComponent(player);
     player->getTransform()->setPosition(glm::vec3(-5, -2, 1));
-    player->getTransform()->setScale(glm::vec3(0.003f, 0.003f, 0.003f));
 
     Entity* club1 = new Entity("club");
     sm.getLoadedScenes()[0]->addEntity(club1);
@@ -121,8 +110,7 @@ int main() {
     Entity* player3 = new Entity("player2");
     sm.getLoadedScenes()[0]->addEntity(player3);
     player3->addComponent(player2);
-    player2->getTransform()->setScale(glm::vec3(0.05f, 0.05f, 0.05f));
-    player2->getTransform()->setPosition(glm::vec3(-1.0f, 7.0f, 0.0f));
+    player->getTransform()->setPosition(glm::vec3(-7, -2, 1));
 
     screenShader.use();
     screenShader.setInt("screenTexture", 0);
@@ -142,27 +130,25 @@ int main() {
     static float mul = 4;
     static float texelSize = 1;
 
-    bool f = true;
     while (!glfwWindowShouldClose(s.window))
-	{
+    {
         imgui_begin();
-		float currentFrame = static_cast<float>(glfwGetTime());
-		s.deltaTime = currentFrame - s.lastFrame;
-		s.lastFrame = currentFrame;
+        float currentFrame = static_cast<float>(glfwGetTime());
+        s.deltaTime = currentFrame - s.lastFrame;
+        s.lastFrame = currentFrame;
         debugInput.interpretInput(s.window, s.camera, s.deltaTime);
         //fb->bind();
 
-        offset += debugInput.interpretIKInput(s.window, s.camera, s.deltaTime);
-        animator->UpdateAnimation(s.deltaTime);
         glClearColor(0.2, 0.2, 0.2, 1);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 
         glm::mat4 projection = glm::perspective(glm::radians(s.camera.Zoom),(float)s.WINDOW_WIDTH / (float)s.WINDOW_HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = s.camera.GetViewMatrix();
+        glm::mat4 view = s.camera.GetViewMatrix();
 
-        //std::cout << sphere->getTransform()->getLocalPosition().x << " " << sphere->getTransform()->getLocalPosition().y << " "<< sphere->getLocalTransform()->getLocalPosition().z << " "<<std::endl;
-		//glm::mat4 projection = playerCamera->getProjection((float)s.WINDOW_WIDTH ,(float)s.WINDOW_HEIGHT);
-		//glm::mat4 view = playerCamera->getView();
+        //std::cout << sphere->getTransform()->getLocalPosition().x << " " << sphere->getTransform()->getLocalPosition().y << " "<< sphere->getTransform()->getLocalPosition().z << " "<<std::endl;
+        //glm::mat4 projection = playerCamera->getProjection((float)s.WINDOW_WIDTH ,(float)s.WINDOW_HEIGHT);
+        //glm::mat4 view = playerCamera->getView();
 
 
 
@@ -183,32 +169,14 @@ int main() {
         }
 
         ImGui::End();
-        imgui_begin();
-        editor.draw();
-        shaderRig.use();
-/*
-        ///IK
-        //playerIK->update(offset);
-        playerRig->update(offset);
-        auto transforms = playerRig->GetFinalBoneMatrices();
-        for (int i = 0; i < transforms.size(); ++i)
-            shaderRig.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
 
-*/
-        ///ANIMATIONS
-        auto transforms = animator->GetFinalBoneMatrices();
-        for (int i = 0; i < transforms.size(); ++i)
-            shaderRig.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
-
-
-        f = false;
         shaderPbr.use();
         shaderPbr.setVec3("camPos",s.camera.Position);
         shaderPbr.setVec3("lightPos",sphere->getTransform()->getLocalPosition());
         ssao.shaderGeometryPass.use();
         ssao.shaderGeometryPass.setBool("invertedNormals", false);
         ssao.shaderGeometryPass.setBool("onlySSAO", onlySSAO);
-		renderer.updateProjectionAndView(projection, view);
+        renderer.updateProjectionAndView(projection, view);
         glBindFramebuffer(GL_FRAMEBUFFER, ssao.gBuffer);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         editor.draw();
@@ -299,7 +267,7 @@ int main() {
         //progressBar.renderBar();
 
         //image.render();
-       // backgroundImage.render();
+        // backgroundImage.render();
         //backgroundImage.update(s.deltaTime);
         //glEnable(GL_DEPTH_TEST);
         //hud end
@@ -310,9 +278,9 @@ int main() {
 //        cm.update();
         //arcadeRenderer->update();
         update();
-	}
+    }
     a.end();
 
-	end();
-	return 0;
+    end();
+    return 0;
 }
