552b7a837ae1a7eba46e93169522752f505b26a6
jakub0ste <127296953+jakub0ste@users.noreply.github.com>
2024-06-17 12:48:53 +0200

    .

--- code/main.cpp
+++ code/main.cpp
@@ -3,6 +3,7 @@
 #include "Globals.h"
 #include "Input/PlayerInput.h"
 #include "Script/exampleSceneScript.h"
+#include "Script/menuSceneScript.h"
 
 
 using namespace SceneManagement;
@@ -13,20 +14,26 @@ int main() {
     glViewport(0,0, 1920, 1080);
 
     //instance
+    menuSceneScript* menusceneScript = new menuSceneScript();
     exampleSceneScript* examplesceneScript = new exampleSceneScript();
     //awake
+    menusceneScript->awake();
     examplesceneScript->awake();
     //start
+    menusceneScript->start();
     examplesceneScript->start();
 
     while (!glfwWindowShouldClose(s.window))
     {
         imgui_begin();
         //update
+        menusceneScript->update();
         examplesceneScript->update();
+        
         update();
     }
     //onDestroy
+    menusceneScript->onDestroy();
     examplesceneScript->onDestroy();
 
 
